6a26b647f7e8f7c14e55a0de916dbb0b53055de9
S-shanker <swaranjalishanker1627@gmail.com>
2024-10-09 15:11:31 +0530

    menu

--- src/main/java/com/flipfit/client/FlipFitGymAdminDashboard.java
+++ src/main/java/com/flipfit/client/FlipFitGymAdminDashboard.java
@@ -1,4 +1,53 @@
 package com.flipfit.client;
+import java.util.Scanner;
 
 public class FlipFitGymAdminDashboard {
+//    static Scanner scanner = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//        System.out.println("-----------------------Welcome to FlipFit Application--------------------");
+//
+//        System.out.println("Press 1 for Login");
+//        System.out.println("Press 2 for Registration for Customer");
+//        System.out.println("Press 3 for Registration for GymOwner");
+//        System.out.println("Press 4 for Update Password");
+//        System.out.println("Press 5 for Exit");
+//        int optionSelected = scanner.nextInt();
+//
+//        switch (optionSelected) {
+//            case 1:
+//                System.out.println("------------Login Page--------------");
+//                System.out.println("Select your role:");
+//                System.out.println("Press 1 for admin");
+//                System.out.println("Press 2 for gym owner");
+//                System.out.println("Press 3 for gym customer");
+//                int role = Integer.parseInt(scanner.nextLine());
+//
+//                switch(role){
+//                    case 1:
+//                        System.out.println("------------Admin Login Page--------------");
+//
+//                    case 2:
+//                        System.out.println("------------Admin Login Page--------------");
+//
+//                    case 3 :
+//                        System.out.println("------------Admin Login Page--------------");
+//                }
+//
+//            case 2:
+//                System.out.println("------------Customer Registration Page--------------");
+//
+//            case 3:
+//                System.out.println("------------Gym Owner Registration Page--------------");
+//
+//            case 4:
+//                System.out.println("------------Password Updation Page--------------");
+//
+//            case 5:
+//                System.out.println("------------Exit--------------");
+//
+//            default: System.out.println("------------Wrong Option selected--------------");
+        }
+
+    }
 }
---
+++ src/main/java/com/flipfit/client/FlipFitGymAdminMenu.java
@@ -0,0 +1,9 @@
+package com.flipfit.client;
+import com.flipfit.bean.Gym;
+import com.flipfit.bean.GymOwner;
+import com.flipfit.bean.User;
+
+
+public class FlipFitGymAdminMenu {
+
+}
