0b2faa58cacfa36a172551211541516735e9b7f9
S-shanker <swaranjalishanker1627@gmail.com>
2024-10-09 14:25:23 +0530

    hello

---
+++ src/main/java/com/flipfit/client/FlipFitGymOwnerClientMenu.java
@@ -0,0 +1,46 @@
+package com.flipfit.client;
+
+import com.flipfit.bean.Gym;
+import com.flipfit.bean.GymOwner;
+import com.flipfit.bean.Slots;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class FlipFitGymOwnerClientMenu {
+    boolean verifyGymOwner(String email, String password) {
+        return true;
+    }
+
+    public boolean gymOwnerLogin(String email, String password){
+        return true;
+    }
+
+    private void addGym(String email){
+
+    }
+
+    private void addSlots(){
+
+    }
+
+    public void createGymOwner(){
+
+    }
+    private boolean updateGymOwnerDetails(){
+        return true;
+    }
+
+    private void displayGyms(String email) {
+    }
+
+    private void updateSeatCount(String email) {
+    }
+
+    public boolean updateGym(String email) {
+        return true;
+    }
+
+}
