43b0d721d6dceebc8ccf6a87ce8db81350c42bd1
kaushik-dey1 <kaushik.dey1@flipkart.com>
2024-10-09 13:06:13 +0530

    [feat]: Added AdminSvc

--- src/main/java/com/flipfit/business/AdminSvc.java
+++ src/main/java/com/flipfit/business/AdminSvc.java
@@ -1,4 +1,16 @@
 package com.flipfit.business;
 
-public class AdminSvc {
+import com.flipfit.bean.Gym;
+import com.flipfit.bean.GymOwner;
+import com.flipfit.bean.User;
+
+import java.util.List;
+
+public interface AdminSvc {
+    List<GymOwner> viewGymOwners();
+    List<Gym> viewGyms();
+    List<User> viewUsers();
+    boolean verifyGym(int gymId);
+    boolean verifyGymOwner(int gymOwnerId);
+    List<Gym> getUnverifiedGyms();
 }
