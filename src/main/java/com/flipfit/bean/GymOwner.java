8483c23a903e9fd0dcf9d633f19db33cd2844602
kaushal.agrawal <kaushala.cm.20@nitj.ac.in>
2024-10-09 12:51:11 +0530

    Added Gym and GymOwner Beans

---
+++ src/main/java/com/flipfit/bean/GymOwner.java
@@ -0,0 +1,89 @@
+package com.flipfit.bean;
+
+import java.util.List;
+
+public class GymOwner {
+
+    private int ownerId;
+    private String ownerEmail;
+    private String password;
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getGST() {
+        return GST;
+    }
+
+    public void setGST(String GST) {
+        this.GST = GST;
+    }
+
+    public List<Gym> getGyms() {
+        return gyms;
+    }
+
+    public void setGyms(List<Gym> gyms) {
+        this.gyms = gyms;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getVerificationStatus() {
+        return verificationStatus;
+    }
+
+    public void setVerificationStatus(String verificationStatus) {
+        this.verificationStatus = verificationStatus;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    private String phoneNo;
+    private String nationalId;
+    private String GST;
+    private List<Gym> gyms;
+    private String ownerName;
+    private String verificationStatus;
+}
